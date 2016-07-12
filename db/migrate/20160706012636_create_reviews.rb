d7a8960a7984386bade2a1c1f167aaad59c5ce26
satsuma-toshiaki <satsuma-toshiaki@outlook.jp>
2016-07-12 17:59:05 +0900

    変更点Ｂ完成(未)

--- db/migrate/20160706012636_create_reviews.rb
+++ db/migrate/20160706012636_create_reviews.rb
@@ -2,6 +2,7 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
       t.string :book_id
+      t.string :user_id
       t.text :body
 
       t.timestamps null: false
