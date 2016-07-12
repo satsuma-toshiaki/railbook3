4a769cbc117ee9b225b23a5dde81e1632df83e39
satsuma-toshiaki <satsuma-toshiaki@outlook.jp>
2016-07-12 17:26:50 +0900

    ユーザー作った

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706012636) do
+ActiveRecord::Schema.define(version: 20160712082407) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 20160706012636) do
   create_table "reviews", force: :cascade do |t|
     t.string   "book_id"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "userid"
+    t.string   "username"
+    t.string   "password"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
